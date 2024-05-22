3cb0a76fa4f26f4262bd835b059b14a8df659491
Charles Kranich <kranichc@roverkids.org>
2024-05-22 13:13:21 -0400

    step 3

--- JumpForce/Jump Force/Assets/Scripts/PlayerController.cs
+++ JumpForce/Jump Force/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
+
     }
 
     // Update is called once per frame
